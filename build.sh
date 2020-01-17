7ae48b762a339e42ee5acd0a34ec5640e97ee247
Bolaji Olajide <cooproton@gmail.com>
2020-01-17 15:42:14 +0100

    feat: release pantry

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-react-native bundle --platform android --dev false --entry-file index.js --bundle-output android/app/src/main/assets/index.android.bundle --assets-dest android/app/src/main/res
+mkdir -p apk
+./gradlew assembleRelease --no-build-cache --stacktrace && cp android/app/build/outputs/apk/release/app-release.apk apk/
